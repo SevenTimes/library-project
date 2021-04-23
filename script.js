32dd6b5ed516fd227782317821b8b2bf49b0ee60
SevenTimes <krab47@yahoo.com>
2021-04-23 20:10:26 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let myLibrary = [];
+const table = document.getElementById('library');
+const btnAddBook = document.getElementById('addBook');
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status; //read or not read yet
+}
+
+let testBook = new Book('Test', 'Me', 1, 'read');
+let testBook2 = new Book('newTest', 'Also Me', 2, 'not read');
+myLibrary.push(testBook, testBook2);
+console.log(testBook);
+console.log(myLibrary);
+
+myLibrary.forEach((book) => {
+  let row = table.insertRow(-1);
+  for (const prop in book) {
+    let newCell = row.insertCell(-1);
+    let newText = document.createTextNode(book[prop]);
+    newCell.appendChild(newText);
+  }
+});
+
+// Popup Form
+
+const formPopup = document.getElementById('form-popup');
+const btnOpen = document.getElementById('btn-open');
+const span = document.getElementsByClassName('close')[0];
+
+btnOpen.onclick = () => (formPopup.style.display = 'block');
+span.onclick = () => (formPopup.style.display = 'none');
+window.onclick = function (event) {
+  if (event.target == formPopup) {
+    formPopup.style.display = 'none';
+  }
+};
+
+// Adding books to array and table
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const pages = document.getElementById('pages');
+const status = document.getElementById('status');
+
+btnAddBook.addEventListener('click', () => {
+  let newBook = new Book(author.value, title.value, pages.value, status.value);
+  myLibrary.push(newBook);
+});
